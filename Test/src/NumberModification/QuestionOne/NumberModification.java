26f60ff8706806198f68bfe52337a18f747c1464
Anubhuti Bhargava <32232830+Anubhuti-Bhargava28@users.noreply.github.com>
2020-03-03 01:36:14 -0800

    Add files via upload

---
+++ Test/src/NumberModification/QuestionOne/NumberModification.java
@@ -0,0 +1,31 @@
+package NumberModification.QuestionOne;
+import java.util.*;
+
+public class NumberModification {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a number :");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+		NumberModification df = new NumberModification();
+		df.Display(num);
+
+	}
+
+	public void Display(int num) {
+		int temp, sum = 0, digit;
+		digit = num % 10;
+		while (num > 0) {
+			temp = num % 10;
+			sum = temp - sum;
+			num /= 10;
+		}
+		if (sum < 0) {
+			System.out.println(-sum + "" + digit);
+		} else {
+			System.out.println(sum + "" + digit);
+		}
+	}
+
+}
